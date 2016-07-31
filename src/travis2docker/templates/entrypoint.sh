7d631d70cd20df9a85e709abdae62a92cd28e4f6
Moisés López <moylop260@vauxoo.com>
2016-07-31 14:16:24 -0500

    [IMP] templates: Support quay.io/travisci/travis-python image

--- src/travis2docker/templates/entrypoint.sh
+++ src/travis2docker/templates/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+{% if image == 'quay.io/travisci/travis-python' -%}
+source /home/travis/virtualenv/python2.7_with_system_site_packages/bin/activate
+{%- endif %}
 {% for entrypoint in entrypoints %}
 {{ entrypoint }}
 {% endfor %}
