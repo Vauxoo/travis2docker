2f99a2a0e0a858defc42472821be1892f03fc8bb
Dave Lasley <dave@laslabs.com>
2016-11-15 17:35:51 -0800

    [ADD] Dockerfile: Support for RVM * Add RVM install * Add bash encapsulation for install commands

--- src/travis2docker/templates/entrypoint.sh
+++ src/travis2docker/templates/entrypoint.sh
@@ -2,6 +2,7 @@
 {% if image == 'quay.io/travisci/travis-python' -%}
 source /home/travis/virtualenv/python2.7_with_system_site_packages/bin/activate
 {%- endif %}
+source /usr/local/rvm/scripts/rvm
 {% for entrypoint in entrypoints %}
 {{ entrypoint }}
 {% endfor %}
