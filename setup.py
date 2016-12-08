8da1ba13bacfd6da22efca73e10f2a76e5a30366
Moisés López <moylop260@vauxoo.com>
2016-12-08 14:47:43 -0600

    Bump version: 3.0.10 → 3.0.11

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='travis2docker',
-    version='3.0.10',
+    version='3.0.11',
     license='BSD',
     description='Script to generate Dockerfile from .travis.yml file',
     long_description='%s\n%s' % (
