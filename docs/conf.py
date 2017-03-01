e1d68636d840f6fa82bb34080b4b87a4beb77c42
Moisés López <moylop260@vauxoo.com>
2017-03-01 10:25:46 -0600

    Bump version: 3.3.1 → 3.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = u'travis2docker'
 year = '2016'
 author = u'Vauxoo'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'3.3.1'
+version = release = u'3.3.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='travis2docker',
-    version='3.3.1',
+    version='3.3.2',
     license='BSD',
     description='Script to generate Dockerfile from .travis.yml file',
     long_description='%s\n%s' % (
