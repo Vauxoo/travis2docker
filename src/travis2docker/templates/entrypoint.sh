0ebb77c9e154717f7ef7ab01e844147711300728
JesusZapata <jesus@vauxoo.com>
2017-09-22 01:44:01 +0000

    [FIX] travis2docker: Change the entrypoint template to use custom python version

--- src/travis2docker/templates/entrypoint.sh
+++ src/travis2docker/templates/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 {% if image == 'quay.io/travisci/travis-python' -%}
-source /home/travis/virtualenv/python2.7_with_system_site_packages/bin/activate
+source /home/travis/virtualenv/python{{ python_version }}_with_system_site_packages/bin/activate
 {% elif  image == 'vauxoo/odoo-80-image-shippable-auto' -%}
-source ${REPO_REQUIREMENTS}/virtualenv/python2.7/bin/activate && source ${REPO_REQUIREMENTS}/virtualenv/nodejs/bin/activate
+source ${REPO_REQUIREMENTS}/virtualenv/python{{ python_version }}/bin/activate && source ${REPO_REQUIREMENTS}/virtualenv/nodejs/bin/activate
 {%- endif %}
 source /rvm_env.sh
 {% for entrypoint in entrypoints %}
