2f27a152fbe541a7f97321d47e74dcb5e49cbe2c
JesusZapata <jesus@vauxoo.com>
2017-09-22 18:54:26 +0000

    [FIX] travis2docker: Fix the name of the python version in travis image

--- src/travis2docker/templates/entrypoint.sh
+++ src/travis2docker/templates/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {% if image == 'quay.io/travisci/travis-python' -%}
-source /home/travis/virtualenv/python{{ python_version }}_with_system_site_packages/bin/activate
+source /home/travis/virtualenv/python{{ python_version }}/bin/activate
 {% elif  image == 'vauxoo/odoo-80-image-shippable-auto' -%}
 source ${REPO_REQUIREMENTS}/virtualenv/python{{ python_version }}/bin/activate && source ${REPO_REQUIREMENTS}/virtualenv/nodejs/bin/activate
 {%- endif %}
