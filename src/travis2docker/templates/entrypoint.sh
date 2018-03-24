ea677b33111d671c9457906739ff747ebed84f56
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2018-03-24 14:17:46 -0600

    [IMP] templates: Using virtualenv for each odoo version to faster installation (#111)
    * [IMP] templates: Using virtualenv for each odoo version for faster installation
    
    * [REF] copy_path: Skip copy errors

--- src/travis2docker/templates/entrypoint.sh
+++ src/travis2docker/templates/entrypoint.sh
@@ -2,7 +2,9 @@
 {% if image == 'quay.io/travisci/travis-python' -%}
 source /home/travis/virtualenv/python{{ python_version }}/bin/activate
 {% elif  image == 'vauxoo/odoo-80-image-shippable-auto' -%}
-source ${REPO_REQUIREMENTS}/virtualenv/python{{ python_version }}/bin/activate && source ${REPO_REQUIREMENTS}/virtualenv/nodejs/bin/activate
+# Compatibility with old images without virtualenv for each odoo version
+ln -s ${REPO_REQUIREMENTS}/virtualenv/python{{ python_version }} ${REPO_REQUIREMENTS}/virtualenv/python{{ python_version }}${VERSION} || true
+source ${REPO_REQUIREMENTS}/virtualenv/python{{ python_version }}${VERSION}/bin/activate && source ${REPO_REQUIREMENTS}/virtualenv/nodejs/bin/activate
 {%- endif %}
 source /rvm_env.sh
 {% for entrypoint in entrypoints %}
