affcbffe2682b9dce6dcded738176060cefe4de6
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2022-09-08 11:54:22 -0500

    [FIX] travis2docker: Fix unshallow repo after change ssh url remote

--- src/travis2docker/docker_helper/build.py
+++ src/travis2docker/docker_helper/build.py
@@ -12,19 +12,27 @@ def ssh_keyscan2known_hosts(url, known_hosts_path=None):
     # python3 -c "import build;build.ssh_keyscan2known_hosts('url')"
     if known_hosts_path is None:
         known_hosts_path = os.path.join(os.path.expanduser("~"), ".ssh", "known_hosts")
-    cmd = ["ssh-keyscan", url]
-    keys_scanned = subprocess.check_output(cmd).decode(sys.stdout.encoding).strip().splitlines()
+
+    # Clear current known hosts
+    cmd = ["ssh-keygen", "-R", url]
+    print(' '.join(cmd))
+    subprocess.call(cmd)
+
+    # Scan new key of host and store
+    cmd = ["ssh-keyscan", "-p", "22", url]
+    print(' '.join(cmd))
+    keys_scanned = subprocess.check_output(cmd).decode(sys.stdout.encoding).strip()
     with open(known_hosts_path, "r+") as known_hosts_f:
-        new_keys_scanned = set(keys_scanned) - set(known_hosts_f.read().splitlines())
-        for key_scanned in new_keys_scanned:
-            known_hosts_f.write("\n" + key_scanned)
+        known_hosts_f.write("\n" + keys_scanned)
 
 
 def git_set_remote(path=None):
     # python3 -c "import build;build.git_set_remote('')"
     if path is None:
-        path = "/home/odoo/instance"
+        # /home/odoo/instance/odoo spends a lot of time
+        path = "/home/odoo/instance/extra_addons"
     git_re = re.compile("([^/|@]+)/([^/]+)/([^/.]+(.git)?)")
+    # TODO: Support ssh url
     path = os.path.join(path, "*", "**", ".git")
     hosts_scanned = set()
     for git_dir in glob.glob(path, recursive=True):
@@ -33,6 +41,7 @@ def git_set_remote(path=None):
         remote = subprocess.check_output(cmd).decode(sys.stdout.encoding).strip().splitlines()[0]
         git_re_match = git_re.search(remote)
 
+        # Configure to fetch all branches (not only the single-branch)
         cmd = git_cmd + ["config", "remote.origin.fetch", "+refs/heads/*:refs/remotes/origin/*"]
         subprocess.call(cmd)
 
@@ -42,7 +51,16 @@ def git_set_remote(path=None):
 
         git_re_groups = git_re_match.groups()
         host = git_re_groups[0]
+        org = git_re_groups[1]
+        repo = git_re_groups[2]
 
+        # Transform https url to ssh format
+        ssh_url_stb = "git@%s:%s/%s" % (host, org, repo)
+        cmd = git_cmd + ["remote", "set-url", "origin", ssh_url_stb]
+        print(' '.join(cmd))
+        subprocess.call(cmd)
+
+        # Unshallow repository
         cmd = git_cmd + ["rev-parse", "--is-shallow-repository"]
         is_shallow = subprocess.check_output(cmd).decode(sys.stdout.encoding).strip().splitlines()[0]
         if is_shallow == 'true':
@@ -53,12 +71,8 @@ def git_set_remote(path=None):
             print(' '.join(cmd))
             subprocess.call(cmd)
 
-        ssh_url_dev = "git@%s:%s-dev/%s" % (host, git_re_groups[1], git_re_groups[2])
+        # Add extra "-dev" remote
+        ssh_url_dev = "git@%s:%s-dev/%s" % (host, org, repo)
         cmd = git_cmd + ["remote", "add", "dev", ssh_url_dev]
-        print(' '.join(cmd))
-        subprocess.call(cmd)
-
-        ssh_url_stb = "git@%s:%s/%s" % (host, git_re_groups[1], git_re_groups[2])
-        cmd = git_cmd + ["remote", "set-url", "origin", ssh_url_stb]
         print(' '.join(cmd))
         subprocess.call(cmd)
