b27a738f20def425a64faf96e3d21a1e4f581507
antonag32 <antonio@vauxoo.com>
2022-12-27 20:06:12 -0600

    FIX - take into account "EXCLUDE" enviroment variable (deployv)
    EXCLUDE was not being taken into account when deciding which modules
    to install and run when using --deployv. Fixed the entrypoint
    to do so

--- src/travis2docker/templates/entrypoint_deployv.sh
+++ src/travis2docker/templates/entrypoint_deployv.sh
@@ -58,7 +58,10 @@ def start_psql():
 def list_modules(modules_path):
     main_repo_path = os.environ["MAIN_REPO_FULL_PATH"]
     list_modules = []
+    excluded_modules = set([module.strip() for module in os.environ.get("EXCLUDE", "").split(",") if module.strip()])
     for i in os.listdir(main_repo_path):
+        if i in excluded_modules:
+          continue
         manifest = os.path.join(main_repo_path, i, "__manifest__.py")
         if os.path.isfile(manifest):
             manifest_data = eval(open(manifest).read())
