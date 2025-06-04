83872fc88ed9af69c77ad991f76175b9ef5cf586
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2025-06-03 20:23:20 -0600

    Bump version: 6.4.17 → 6.4.18

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = u'travis2docker'
 year = '2016'
 author = u'Vauxoo'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'6.4.17'
+version = release = u'6.4.18'
 
 pygments_style = 'trac'
 templates_path = ['.']
