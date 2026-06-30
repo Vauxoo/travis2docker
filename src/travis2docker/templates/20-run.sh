0933fd659d7bfee62a6900629431358aa5378a6c
Luis González <lgonzalez@vauxoo.com>
2026-06-30 11:33:43 -0700

    [FIX] travis2docker: automatically fix DeployV file ownership
    Some files under `/home/odoo` are owned by root or other users due to
    declared volumes and files shared into the container. This required
    users to manually restore their ownership after starting the container.
    
    To fix this, run the ownership correction automatically from
    `20-run.sh` once the container is running, and remove the corresponding
    manual instructions from the generated output.
    
    Closes #217

--- src/travis2docker/templates/20-run.sh
+++ src/travis2docker/templates/20-run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 export IMAGE={{ image }}
-docker run {{ extra_params }} $1 -itP $IMAGE $2
+CONTAINER=$(docker run {{ extra_params }} $1 -ditP $IMAGE $2)
+
+# Some files under /home/odoo end up owned by root or other users (declared VOLUMEs and
+# files shared into the container); fix their ownership in the background so we can attach ASAP
+docker exec --user=root "$CONTAINER" find /home/odoo -maxdepth 2 -not -user odoo -exec chown -R odoo:odoo {} + &
+docker attach "$CONTAINER"
 {{ extra_cmds }}
